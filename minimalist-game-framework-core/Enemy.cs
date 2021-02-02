92f8bf04747a4119d9599ee7d65c34caf66e85dd
Aaryan <s-aajain@lwsd.org>
2021-02-01 23:04:42 -0800

    Make skeleton for enemy.cs
    Implemented some functions for enemy.cs
    
    Might need to implement more

---
+++ minimalist-game-framework-core/Enemy.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Enemy
+    {
+        Boolean death;
+        Vector2 location;
+        Boolean enemyType;
+        Texture enemy1 = Engine.LoadTexture("enemy1.png");
+        Texture enemy2 = Engine.LoadTexture("enemy2.png");
+        int health;
+
+        public void makeEnemy(Vector2 location)
+        {
+            this.location = location;
+            Random random = new System.Random();
+            if(random.Next(0, 100) > 50)
+            {
+                enemyType = true;
+                health = 1;
+                Engine.DrawTexture(enemy1, location);
+            }
+            else
+            {
+                enemyType = false;
+                health = 2;
+                Engine.DrawTexture(enemy2, location);
+
+            }
+            death = false;
+        }
+
+        public Vector2 getLocation()
+        {
+            return location;
+        }
+
+        public void setLocation(Vector2 location)
+        {
+            this.location = location;
+        }
+
+        
+
+        public Boolean isDead()
+        {
+            if (health == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
